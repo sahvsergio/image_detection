6a77f235028022c307d289386517e4ad60a995c8
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2026-08-02 21:57:52 -0500

    change photo to result and plot it to see objected detected

--- image_detector.py
+++ image_detector.py
@@ -12,7 +12,7 @@ photo=cv2.imread("3586.jpg") ## reads the photo path
 ## Add the photo to the model for detection
 result=model(photo)
 print(result)    
-"""cv2.imshow("my window", photo)## shows the photo and assigns the name
+cv2.imshow("my window", result[0].plot())## shows the photo and assigns the name
 ## of the window and provides the photo to be shown
 
 ## assigns the position of the window on the screen
@@ -24,4 +24,3 @@ cv2.waitKey(5000)
 
 ## Destroy all the windows created by OpenCV
 cv2.destroyAllWindows()
-"""
