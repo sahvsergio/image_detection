05d17516734fe47bfa2519544075ec386c699bef
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2026-08-02 21:30:05 -0500

    assign windwo and photo to be shown to imgshow

--- image_detector.py
+++ image_detector.py
@@ -4,3 +4,5 @@ import cv2
 # create an instance of the photo 
 
 photo=cv2.imread("3586.jpg") ## reads the photo path
+cv2.imshow("my window", photo)## shows the photo and assigns the name
+## of the window and provides the photo to be shown
