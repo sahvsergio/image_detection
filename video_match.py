864a573af330d9df6e037a6ee8efd38e2a376cf2
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2026-09-01 20:58:02 -0500

    add test code for image showing properly

--- video_match.py
+++ video_match.py
@@ -17,3 +17,13 @@ folder= "faces/Mariya"
 
 #list all files inside the designated faces folder
 files =os.listdir(folder)
+
+# load each file with openCV
+
+for file in files:
+
+    photo=cv2.imread(folder+"/"+file)
+
+#test code to verify images are being read correctly
+Image.fromarray(photo[:,:,::-1]).show()
+
